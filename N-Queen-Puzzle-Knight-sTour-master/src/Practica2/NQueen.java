68c450f6950fe52088f4b39fcdf663102d27b3fa
Andrea <andreamoreysanchez@gmail.com>
2020-01-03 21:05:49 +0100

    ACABADO CHICAAAAAAAAS

---
+++ N-Queen-Puzzle-Knight-sTour-master/src/Practica2/NQueen.java
@@ -0,0 +1,14 @@
+
+package Practica2;
+
+public class NQueen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainPage mp = new MainPage();
+        mp.main(new String[0]);
+    }
+    
+}
---
+++ N-Queen-Puzzle-Knight-sTour-master/src/Practica2/Queen.java
@@ -0,0 +1,153 @@
+package Practica2;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Queen extends JPanel {
+    private static int dimension, dim;
+    private static char[][] t;
+    private static int size = 35; // size of the cell
+    private static int margin = 80;   // margin of the board
+    private static int x;
+    private static int y;
+    private static int contador;
+    public static JFrame frame;
+     
+      public void paint(Graphics g){
+        int d = dimension;
+        Image img1 = Toolkit.getDefaultToolkit().getImage("src/pic_queen.png");
+        Image img2 = Toolkit.getDefaultToolkit().getImage("src/queenB.png");
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(margin, margin, dim, dim);
+        if(d == 1)
+             g.drawImage(img1, margin, margin, size, size , null , this);
+        else{
+            for(int i = margin; i < dim + margin; i+=2*size){
+                for(int j = margin; j < dim + margin; j+=2*size){
+                    g.clearRect(i, j, size, size);
+                }
+            }
+
+            for(int i = margin+size; i < dim + margin; i+=2*size){
+                for(int j = margin + size; j < dim + margin; j+=2*size){
+                    g.clearRect(i, j, size, size);
+                }
+            }
+        }
+        
+        
+        for(int i = 0; i < d; i++){
+            for(int j = 0; j < d; j++){
+                if(t[i][j] == 'Q'){
+                    
+                    g.drawImage(img1, margin + i*size, margin + j * size, size, size , null , this); 
+                }
+                if(t[i][j] == 'q'){
+                    
+                    g.drawImage(img2, margin + i*size, margin + j * size, size, size , null , this); 
+                }
+            }
+        }
+    }
+    public Queen() {
+        initComponents();
+    }
+    
+    public static int main(String[] args){
+    dimension = Integer.parseInt(args[0]);
+    size = Integer.parseInt(args[1]);
+    x = (Integer.parseInt(args[2]))-1;
+    y = (Integer.parseInt(args[3]))-1;
+    
+    dim = dimension * size;
+    frame = new JFrame();
+    frame.setTitle("Problema de las n reinas");
+    frame.setSize(dim+2* + margin, dim+2* + margin + size / 2);
+    frame.getContentPane().add(new Queen());
+    frame.setLocationRelativeTo(null);
+    frame.setBackground(Color.WHITE);
+    frame.setVisible(true);
+    t = new char[dimension][dimension];
+    enumerate(dimension);
+    return contador;
+    }
+    
+    public static boolean isConsistent(int[] q, int n) {
+        for (int i = 0; i < n; i++) {
+            if (q[i] == q[n]){  
+                return false;
+            }   
+            if ((q[i] - q[n]) == (n - i)){ 
+                return false;
+            }   
+            if ((q[n] - q[i]) == (n - i)){ 
+                return false;
+            }
+        }
+        return true;
+    }
+
+   public static void printTable(int[] q) {
+        int n = q.length;
+        contador = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (q[i] == j){
+                    t[i][j] = 'Q';
+                    t[y][x] = 'q';      //los he tenido que girar para que vaya bien
+                    contador ++;
+                }
+                else{
+                    t[i][j] = '*';
+                }
+            }
+        } 
+    }
+
+   public static void enumerate(int n) {
+        int[] a = new int[n];
+        enumerate(a, 0);
+        
+    }
+
+    public static void enumerate(int[] q, int k) {
+        int n = q.length;
+        if (k == n){
+            printTable(q);
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                q[k] = i;
+                q[y] = x;
+                    if (isConsistent(q,k)){
+                        enumerate(q, k+1);
+                    }
+                }
+        }
+    }  
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 705, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 633, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
